1a309120eb290f01ee8c17ca7a3a00fbafc68887
Alejandro Arias Jimenez <alejandro.ariasjimenez@cea.fr>
2025-12-08 16:00:49 +0100

    Interface in python

--- python_src/new_interface.py
+++ python_src/new_interface.py
@@ -1,35 +1,32 @@
-#Example of a new interface to run the code.
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import h5py
+import facitpy
 
+# ------------------------------
+# Part 1: Inputs and profiles
+# ------------------------------
 
-
-
-#Part 1 Prepare the inputs:
-
-#numbers
-    #integers
-nx = 1000 #size of radial arrays
-nth = 1000 #size of poloidal arrays
-nions = 5  #number of ion species
+nx = 100
+nth = 100
+nions = 5
 Aa = 184
 
-    #reals
+mp = 1.67262192369e-27
+q_e = 1.602176634e-19
 invaspct = 0.2
-B0= 3.7
+B0 = 3.7
 R0 = 2.5
-qa=4
+qa = 4
+ma = Aa*mp
+cor = 1e-33
 
+xn = np.linspace(0,1,nx)
+theta = np.linspace(0,2*np.pi,nth)
 
-#Vectors
-
-
-xn = np.arange(0, 1, nx)
-theta = np.arange(0, 2*np.pi, nth)
 Za = np.zeros(nx)
-Ai = np.zeros(nions)
+Ai = np.array([2,3,4,14,16])
 Te = np.zeros(nx)
 Ti = np.zeros(nx)
 Ne = np.zeros(nx)
@@ -40,205 +37,135 @@ gradNa = np.zeros(nx)
 qmag = np.zeros(nx)
 dpsidx = np.zeros(nx)
 FV = np.zeros(nx)
-BV = np.zeros(nx)
-RV = np.zeros(nx)
-regulopt = np.zeros(4)
+regulopt = np.array([0,0,0,0],dtype=np.float64)
 
-
-# Matrix.
-
-Zi = np.zeros((nx, nions))
-Ni = np.zeros((nx, nions))
-gradNi = np.zeros((nx, nions))
-jacob = np.zeros((nx, nth))
-AsymPhi = np.zeros((nx, nth))
-
-#Tensor.
-
-AsymN =  np.zeros((nx, nth, nions))
-
-
-#logical
+BV = np.zeros((nx,nth))
+RV = np.zeros((nx,nth))
+Zi = np.zeros((nx,nions))
+Ni = np.zeros((nx,nions))
+gradNi = np.zeros((nx,nions))
+jacob = np.zeros((nx,nth))
+AsymPhi = np.zeros((nx,2))
+AsymN = np.zeros((nx,nions,2))
 
 pol_asym = True
 rotation = True
 solution = 3
 full_geom = True
 
-#extra 
-ni0 = [4e19, 1e19, 1e14, 5e9, 2e10] #select inicial density for each case
+ni0 = [4e19,1e19,1e14,5e9,2e10]
 
+Ai = np.array([2,3,4,14,16])
 
-#Profiles
+Ni[:,0] = ni0[0]*(1-xn**2)**2
+Ni[:,1] = ni0[1]*(1-xn**2)**2
+Ni[:,2] = ni0[2]*(1-xn**2)**2
+Ni[:,3] = ni0[3]*(0.3+np.exp(6*(xn-1)))
+Ni[:,4] = ni0[4]*(0.3+np.exp(6*(xn-1)))
 
-Ai = [2,3,4,14,16]
+Na = 6e12*(0.3+np.exp(6*(xn-1)))
+Ne = 6e19*(1-xn**2)**2
 
-Ni[:,0] = ni0[0] * (1 - xn**2)**2 #Deuterium
-Ni[:,1] = ni0[1]* (1 - xn**2)**2 #Helium or T
-Ni[:,2] = ni0[2]* (1 - xn**2)**2 #Helium or T
-Ni[:,3] = ni0[3]* (0.3 + np.exp(6*(xn-1))) #light impurities
-Ni[:,4] = ni0[4]* (0.3 + np.exp(6*(xn-1))) #light impurities
+Zi[:,0] = 1
+Zi[:,1] = 1
 
-Na = 6e12 * (0.3 + np.exp(6*(xn-1)))
+for i in range(nx):
+    Zi[i,2] = 1 if xn[i]>=0.6 else 2
+    FV[i] = R0*B0
 
-Ne = 6e19 * (1 - xn**2)**2
+Zi[:,3] = 6 - 5*np.exp(-((xn-1)/0.25)**2)
+Zi[:,4] = 7 - 6*np.exp(-((xn-1)/0.25)**2)
 
-Zi[:,0] = 1 #Deuterium
-Zi[:,1] = 1 # T
-#definir ifs
-if xn>=0.6:
-    Zi[:,2] = 1 #Helium
-else:
-    Zi[:,2] = 2
+Za = 60 - 55*np.exp(-((xn-1)/0.3)**2)
 
-Zi[:,3] = 6 - 5 * np.exp(-((xn - 1) / 0.25)**2)
-Zi[:,4] = 7 - 6 * np.exp(-((xn - 1) / 0.25)**2)
-
-
-
-Zimp = 60 - 55 * np.exp(-((xn - 1) / 0.3)**2)
-
-
-qmag =1 + (qa-1)*xn**2
-
-Btheta = 0.5 * (xn / qmag)
-Btheta[0] = 0.0 
-
-
+qmag = 1 + (qa-1)*xn**2
 Ti = 1.5*(1-xn**2)**2
+Te = 3*(1-xn**2)**2
 
-Te = 3 *(1-xn**2)**2
+for j in range(nth):
+    RV[:,j] = xn*invaspct*np.cos(theta[j])
+    BV[:,j] = FV**2/(RV[:,j]**2+cor)*(1+invaspct**2/(RV[:,j]**2+cor)*xn**2/(qmag**2+cor))
+    jacob[:,j] = xn / (B0*(1-invaspct*np.cos(theta[j])) + cor)
 
-#Gradients
+S1 = 0
+S2 = Na
+for j in range(nions):
+    S1 += Ni[:,j]*Ai[j]*mp
+    S2 += Ni[:,j]
 
-gradTi = np.grad(Ti,xn)
-gradNa = np.grad(Na,xn)
+meff = S1/S2
+eps = xn/R0
+anis = Te/(Ti+cor)-1
+Omega = Za * q_e * B0/(Aa*mp)
+Machi = (ma*Na+S1)/(S2*2*Ti + cor)*R0*Omega
 
-for i in range(len(xn)):
-    gradNi[:,i]= np.grad(Ni[:,i],xn)
+for i in range(nx):
+    dpsidx[i] = (2*xn[i]*qmag[i] - xn[i]**2*(2*(4-1)*xn[i]))/(qmag[i]**2+cor)
 
-for i in range(len(FV)):
-    FV[i]= R0*B0
+gradTi = np.gradient(Ti,xn)
+gradNa = np.gradient(Na,xn)
+for i in range(nions):
+    gradNi[:,i] = np.gradient(Ni[:,i],xn)
 
-#BV=
-#RV=
-#jacob= 
-#AsymPhi=
-#AsymN= 
+AsymPhi[:] = 0
+AsymN[:] = 1
 
+# ------------------------------
+# Part 2: Normalize + Fortran call
+# ------------------------------
 
-#Part 2 Comunicate with fortran (We can use f2py)
+def arrF(a): return np.asfortranarray(np.asarray(a, dtype=np.float64))
 
-import facit_code
+raw_outputs = facitpy.facit_mod.facit(
+    arrF(xn), arrF(theta), arrF(Za), float(Aa), arrF(Zi), arrF(Ai),
+    arrF(Te), arrF(Ti), arrF(Ne), arrF(Ni), arrF(Na), arrF(Machi),
+    arrF(gradTi), arrF(gradNi), arrF(gradNa),
+    float(invaspct), float(B0), float(R0), arrF(qmag), arrF(dpsidx),
+    arrF(FV), arrF(BV), arrF(RV), arrF(jacob),
+    arrF(AsymPhi), arrF(AsymN),
+    int(pol_asym), int(full_geom), int(solution), int(rotation),
+    arrF(regulopt)
+)
 
-#should be enough with the f2py but to be determined.
-
-
-
-#Part 3 Save the info.
-
+# ------------------------------
+# Part 3: Output handling
+# ------------------------------
 
 class FACITResult:
-    def __init__(self, outputs):
-
+    def __init__(self, out):
         (
-            self.Flux_imp, self.Da, self.Da_M, self.Vconv,
-            self.nn, self.dmin, self.dmaj,
-
+            self.Flux_imp, self.Da, self.Da_M,
+            self.Vconv, self.Vconv_M, self.nn,
+            self.dmin, self.dmaj,
             self.Da_PS_M, self.Da_BP_M, self.Da_CL_M,
             self.Ka_PS_M, self.Ka_BP_M, self.Ka_CL_M,
             self.Ha_PS_M, self.Ha_BP_M, self.Ha_CL_M,
             self.Va_PS_M, self.Va_BP_M, self.Va_CL_M,
-
             self.Da_PS, self.Da_BP, self.Da_CL,
             self.Ka_PS, self.Ka_BP, self.Ka_CL,
             self.Ha_PS, self.Ha_BP, self.Ha_CL,
             self.Va_PS, self.Va_BP, self.Va_CL
-        ) = outputs
+        ) = out
 
-        # Dictionary per category.
-        self.Da_comp = {
-            "PS": self.Da_PS,
-            "BP": self.Da_BP,
-            "CL": self.Da_CL,
-            "PS_M": self.Da_PS_M,
-            "BP_M": self.Da_BP_M,
-            "CL_M": self.Da_CL_M,
-        }
-
-        self.Ka_comp = {
-            "PS": self.Ka_PS,
-            "BP": self.Ka_BP,
-            "CL": self.Ka_CL,
-            "PS_M": self.Ka_PS_M,
-            "BP_M": self.Ka_BP_M,
-            "CL_M": self.Ka_CL_M,
-        }
-
-        self.Ha_comp = {
-            "PS": self.Ha_PS,
-            "BP": self.Ha_BP,
-            "CL": self.Ha_CL,
-            "PS_M": self.Ha_PS_M,
-            "BP_M": self.Ha_BP_M,
-            "CL_M": self.Ha_CL_M,
-        }
-
-        self.Va_comp = {
-            "PS": self.Va_PS,
-            "BP": self.Va_BP,
-            "CL": self.Va_CL,
-            "PS_M": self.Va_PS_M,
-            "BP_M": self.Va_BP_M,
-            "CL_M": self.Va_CL_M,
-        }
-
-raw_outputs = facit_code.facit( nx, nth, xn, nions, theta, Za, Aa, Zi, Ai,
-    Te, Ti, Ne, Ni, Na, Machi, gradTi, gradNi, gradNa, invaspct, B0, R0, qmag, dpsidx,
-    FV, BV, RV, jacob, AsymPhi, AsymN, pol_asym, full_geom, solution, rotation, regulopt)
+        self.Da_comp = {"PS":self.Da_PS,"BP":self.Da_BP,"CL":self.Da_CL,
+                        "PS_M":self.Da_PS_M,"BP_M":self.Da_BP_M,"CL_M":self.Da_CL_M}
 
 result = FACITResult(raw_outputs)
 
-def to_hdf5(self, filename="facit_output.h5"):
-    with h5py.File(filename, "w") as f:
-        f["Flux_imp"] = self.Flux_imp
-        f["Da"] = self.Da
-        f["Da_M"] = self.Da_M
-        f["Vconv"] = self.Vconv
-        f["nn"] = self.nn
-        f["dmin"] = self.dmin
-        f["dmaj"] = self.dmaj
+# ------------------------------
+# Save
+# ------------------------------
+with h5py.File("results_facit.h5","w") as f:
+    f["Da"] = result.Da
+    f["Flux_imp"] = result.Flux_imp
+print("saved in results_facit.h5")
 
-        grp_Da = f.create_group("Da_comp")
-        for k, v in self.Da_comp.items():
-            grp_Da[k] = v
-        
-        grp_Ka = f.create_group("Ka_comp")
-        for k, v in self.Ka_comp.items():
-            grp_Ka[k] = v
-        
-        grp_Ha = f.create_group("Ha_comp")
-        for k, v in self.Ha_comp.items():
-            grp_Ha[k] = v
-        
-        grp_Va = f.create_group("Va_comp")
-        for k, v in self.Va_comp.items():
-            grp_Va[k] = v
-
-#Part 3 Plots.
-
-
-
-def plot_profile(x, y, xlabel="", ylabel="", title=""):
-    plt.figure(figsize=(6,4))
-    sns.lineplot(x=x, y=y)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.title(title)
-    plt.grid(True)
-    plt.tight_layout()
-    plt.show()
-
-plot_profile( x=xn, y=result.Da, xlabel="r", ylabel="Da", title="Particle Diffusivity Da(x)")
-plot_profile( x=result.dmin, y=result.dmaj, xlabel="delta h", ylabel="Delta V", title="Assymetry")
+# ------------------------------
+# Plots
+# ------------------------------
+plt.figure()
+plt.plot(xn, result.Da)
+plt.title("Da(x)")
+plt.grid()
+plt.show()
 
